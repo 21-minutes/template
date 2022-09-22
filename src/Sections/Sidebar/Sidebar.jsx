49526290388c6b22f9bca524979096250482510b
Janis Ringli <janis.ringli@gmail.com>
2022-09-22 18:31:27 +0200

    add sidebar

---
+++ src/Sections/Sidebar/Sidebar.jsx
@@ -0,0 +1,13 @@
+import "./Sidebar.scss";
+
+function SidebarLeft() {
+  return (
+    <div className="sidebar-left__container">
+      <div className="template-sidebar-left">
+        <div className="sidebar-left-logo">logo</div>
+        <div className="sidebar-left-nav"></div>
+      </div>
+    </div>
+  );
+}
+export default SidebarLeft;
