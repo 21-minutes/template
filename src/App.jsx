4ecca2c10662414832a9eae3eee10b8af92747fd
Janis Ringli <janis.ringli@gmail.com>
2022-09-27 00:27:44 +0200

    admin panel at work

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import "./App.scss";
 import SidebarLeft from "./Sections/Sidebar/Sidebar";
 import Content from "./Sections/Content/Content";
 import ProfileSection from "./Sections/ProfileSection/ProfileSection";
+import Admin from "./Sections/Admin/Admin";
 
 function App() {
   return (
     <div className="app-container">
-      <SidebarLeft />
+      <Admin></Admin>
+      {/* <SidebarLeft />
       <Content />
-      <ProfileSection />
+      <ProfileSection /> */}
     </div>
   );
 }
--- src/Sections/Admin/Admin.jsx
+++ src/Sections/Admin/Admin.jsx
@@ -1,9 +1,25 @@
-import "./Admin.css";
+import "./Admin.scss";
 
 function Admin() {
+  function sendIt() {
+    console.log("send it");
+  }
   return (
     <div className="admin-container">
       <h1>Admin</h1>
+      <input type="text" placeholder="Authorname" className="test" />
+      <textarea type="text" placeholder="Author description" className="test" />
+      <input type="file" placeholder="Avatar" />
+
+      <input type="number" placeholder="Readingtime" className="test" />
+      <input type="text" placeholder="Title" className="test" />
+      <input type="text" placeholder="Subtitle" className="test" />
+      <textarea type="text" placeholder="Content" className="test" />
+      <input type="Date" placeholder="Date" className="test" />
+      <button onClick={sendIt} type="submit">
+        Submit
+      </button>
     </div>
   );
 }
+export default Admin;
