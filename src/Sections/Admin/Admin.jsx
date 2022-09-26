e02530e3d48653b1351f92671e2e355d67a03441
“Janis <janis.ringli@gmail.com>
2022-09-27 01:03:03 +0200

    add all current endpoints
    not clean at all though, maybe i will update the styling a bit when its functional

--- src/Sections/Admin/Admin.jsx
+++ src/Sections/Admin/Admin.jsx
@@ -1,21 +1,113 @@
 import "./Admin.scss";
+import { useState } from "react";
 
 function Admin() {
+  const [authorName, setAuthorName] = useState("");
+  const [readingTime, setReadingTime] = useState("");
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+  const [date, setDate] = useState("");
+  const [avatar, setAvatar] = useState("");
+  const [authorDescription, setAuthorDescription] = useState("");
+
+  const handleChangeAuthorName = (event) => {
+    setAuthorName(event.target.value);
+  };
+  const handleChangeReadingTime = (event) => {
+    setReadingTime(event.target.value);
+  };
+  const handleChangeTitle = (event) => {
+    setTitle(event.target.value);
+  };
+  const handleChangeText = (event) => {
+    setText(event.target.value);
+  };
+  const handleChangeDate = (event) => {
+    setDate(event.target.value);
+  };
+  const handleChangeAvatar = (event) => {
+    setAvatar(event.target.value);
+  };
+  const handleChangeAuthorDescription = (event) => {
+    setAuthorDescription(event.target.value);
+  };
   function sendIt() {
-    console.log("send it");
+    console.log("Authorname: " + authorName);
+    console.log("Readingtime: " + readingTime);
+    console.log("Author Description: " + authorDescription);
+    console.log("Title: " + title);
+    console.log("Text: " + text);
+    console.log("Date: " + date);
+    console.log("Avatar: " + avatar);
   }
   return (
     <div className="admin-container">
       <h1>Admin</h1>
-      <input type="text" placeholder="Authorname" className="test" />
-      <textarea type="text" placeholder="Author description" className="test" />
-      <input type="file" placeholder="Avatar" />
+      <input
+        autoComplete="off"
+        type="text"
+        placeholder="Authorname"
+        className="test"
+        id="authorName"
+        name="authorName"
+        onChange={handleChangeAuthorName}
+        value={authorName}
+      />
+      <h2>{authorName}</h2>
+      <textarea
+        type="text"
+        placeholder="Author description"
+        className="test"
+        onChange={handleChangeAuthorDescription}
+        name="authorDescription"
+        value={authorDescription}
+        id="AuthorDescription"
+      />
+      <input
+        type="file"
+        placeholder="Avatar"
+        name="avatar"
+        id="avatar"
+        onChange={handleChangeAvatar}
+        value={avatar}
+      />
 
-      <input type="number" placeholder="Readingtime" className="test" />
-      <input type="text" placeholder="Title" className="test" />
-      <input type="text" placeholder="Subtitle" className="test" />
-      <textarea type="text" placeholder="Content" className="test" />
-      <input type="Date" placeholder="Date" className="test" />
+      <input
+        type="number"
+        placeholder="Readingtime"
+        className="test"
+        name="readingTime"
+        id="readingTime"
+        onChange={handleChangeReadingTime}
+        value={readingTime}
+      />
+      <input
+        type="text"
+        placeholder="Title"
+        className="test"
+        name="title"
+        id="title"
+        onChange={handleChangeTitle}
+        value={title}
+      />
+      <textarea
+        type="text"
+        placeholder="Content"
+        className="test"
+        name="text"
+        id="text"
+        onChange={handleChangeText}
+        value={text}
+      />
+      <input
+        type="Date"
+        placeholder="Date"
+        className="test"
+        name="date"
+        id="date"
+        onChange={handleChangeDate}
+        value={date}
+      />
       <button onClick={sendIt} type="submit">
         Submit
       </button>
