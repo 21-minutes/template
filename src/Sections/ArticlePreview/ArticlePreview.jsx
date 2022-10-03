b1702cd24dcb50b09f670aad43e98a0f1b73dca1
“Janis <janis.ringli@gmail.com>
2022-10-04 01:12:44 +0200

    finish home page with variable content that will come from the api

--- src/Sections/ArticlePreview/ArticlePreview.jsx
+++ src/Sections/ArticlePreview/ArticlePreview.jsx
@@ -4,13 +4,16 @@ function ArticlePreview(props) {
   return (
     <div className="article-preview__container">
       <div className="template-article-preview">
-        test
+        <div className="article-preview-header">
+          <div className="header-avatar"></div>
+          <div className="header-author">{props.author}</div>
+          <div className="header-date">{props.date}</div>
+        </div>
         <div className="article-preview-title">{props.title}</div>
-        <div className="articel-preview-text">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut quis
-          quaerat ducimus modi praesentium numquam, esse atque reprehenderit
-          repellendus, ullam cum eligendi quae qui in at, facere eius similique
-          soluta?
+        <div className="articel-preview-text">{props.text}</div>
+        <div className="article-preview-footer">
+          <div className="footer-topic">{props.topic}</div>
+          <div className="footer-reading-time">{props.readingTime}</div>
         </div>
       </div>
     </div>
--- src/Sections/HomeContent/HomeContent.jsx
+++ src/Sections/HomeContent/HomeContent.jsx
@@ -3,10 +3,85 @@ import ArticlePreview from "../ArticlePreview/ArticlePreview";
 function Content() {
   return (
     <div className="home-content__container">
+      <div className="home-content-header"></div>
       <div className="template-home-content">
-        <div className="home-content-header">
-          <ArticlePreview title="Test"></ArticlePreview>
-        </div>
+        <ArticlePreview
+          title="Test"
+          author="Janis Ringli"
+          date="Oct 4"
+          readingTime="5 min"
+          topic="React"
+          text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut quis
+          quaerat ducimus modi praesentium numquam, esse atque reprehenderit
+          repellendus, ullam cum eligendi quae qui in at, facere eius similique
+          soluta?"
+        ></ArticlePreview>
+        <ArticlePreview
+          title="Test"
+          author="Janis Ringli"
+          date="Oct 4"
+          readingTime="5 min"
+          topic="React"
+          text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut quis
+          quaerat ducimus modi praesentium numquam, esse atque reprehenderit
+          repellendus, ullam cum eligendi quae qui in at, facere eius similique
+          soluta?"
+        ></ArticlePreview>
+        <ArticlePreview
+          title="Test"
+          author="Janis Ringli"
+          date="Oct 4"
+          readingTime="5 min"
+          topic="React"
+          text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut quis
+          quaerat ducimus modi praesentium numquam, esse atque reprehenderit
+          repellendus, ullam cum eligendi quae qui in at, facere eius similique
+          soluta?"
+        ></ArticlePreview>
+        <ArticlePreview
+          title="Test"
+          author="Janis Ringli"
+          date="Oct 4"
+          readingTime="5 min"
+          topic="React"
+          text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut quis
+        quaerat ducimus modi praesentium numquam, esse atque reprehenderit
+        repellendus, ullam cum eligendi quae qui in at, facere eius similique
+        soluta?"
+        ></ArticlePreview>
+        <ArticlePreview
+          title="Test"
+          author="Janis Ringli"
+          date="Oct 4"
+          readingTime="5 min"
+          topic="React"
+          text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut quis
+          quaerat ducimus modi praesentium numquam, esse atque reprehenderit
+          repellendus, ullam cum eligendi quae qui in at, facere eius similique
+          soluta?"
+        ></ArticlePreview>
+        <ArticlePreview
+          title="Test"
+          author="Janis Ringli"
+          date="Oct 4"
+          readingTime="5 min"
+          topic="React"
+          text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut quis
+          quaerat ducimus modi praesentium numquam, esse atque reprehenderit
+          repellendus, ullam cum eligendi quae qui in at, facere eius similique
+          soluta?"
+        ></ArticlePreview>
+        <ArticlePreview
+          title="Test"
+          author="Janis Ringli"
+          date="Oct 4"
+          readingTime="5 min"
+          topic="React"
+          text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut quis
+          quaerat ducimus modi praesentium numquam, esse atque reprehenderit
+          repellendus, ullam cum eligendi quae qui in at, facere eius similique
+          soluta?"
+        ></ArticlePreview>
       </div>
     </div>
   );
