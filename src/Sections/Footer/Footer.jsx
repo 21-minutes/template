b0f9f1586c457489a5ccfc1a9201b3d3f374fe11
Janis Ringli <janis.ringli@gmail.com>
2022-10-05 15:07:05 +0200

    add searchicon to footer

--- src/Sections/Footer/Footer.jsx
+++ src/Sections/Footer/Footer.jsx
@@ -1,12 +1,14 @@
 import "./Footer.scss";
 import houseIcon from "../../assets/house.svg";
+import searchIcon from "../../assets/search.svg";
 
 function Footer() {
   return (
     <div className="footer-container">
       <div className="container-icons">
         <img className="icon-item" src={houseIcon}></img>
-        <div className="icon-item"></div>
+        <img className="icon-item" src={searchIcon}></img>
+
         <div className="icon-item"></div>
       </div>
     </div>
