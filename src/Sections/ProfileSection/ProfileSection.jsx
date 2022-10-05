a39fd7ae5e87aa784b19966c99b3adbbd85b897c
Janis Ringli <janis.ringli@gmail.com>
2022-10-05 12:24:13 +0200

    add searchinput to profilesection

--- src/Sections/ProfileSection/ProfileSection.jsx
+++ src/Sections/ProfileSection/ProfileSection.jsx
@@ -1,10 +1,14 @@
 import "./ProfileSection.scss";
 import AboutAuthor from "../../components/AboutAuthor/AboutAuthor";
+import SearchInput from "../../components/SearchInput/SearchInput";
 
 function ProfileSection() {
   return (
     <div className="profile-section__container">
-      <AboutAuthor />
+      <div className="profile-section__content">
+        <SearchInput></SearchInput>
+        <AboutAuthor />
+      </div>
     </div>
   );
 }
