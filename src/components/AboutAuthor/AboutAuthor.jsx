55f6f191fcc7c96129867445947da51257d72a44
Janis Ringli <janis.ringli@gmail.com>
2022-10-05 12:23:07 +0200

    hardcode some stuff to see if it looks good, will be replaced as soon as the api is live

--- src/components/AboutAuthor/AboutAuthor.jsx
+++ src/components/AboutAuthor/AboutAuthor.jsx
@@ -1,6 +1,7 @@
 import "./AboutAuthor.scss";
 import { getAllUsers } from "../../Model/Author";
 import { useEffect, useState } from "react";
+import avatar from "../../assets/people/janis_ringli.jpg";
 
 function AboutAuthor() {
   const [allUsers, setAllUsers] = useState();
@@ -15,8 +16,16 @@ function AboutAuthor() {
   console.log(allUsers?.attributes.name);
   return (
     <div className="about-author__container">
-      <div className="container-avatar"></div>
-      <div className="container-name">{allUsers?.attributes.name}</div>
+      <img className="container-avatar" src={avatar}></img>
+      <div className="container-name">
+        {allUsers?.attributes.name}Janis Ringli
+      </div>
+      <div className="container-bio">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
+        accusantium asperiores corporis accusamus dolorum ex, reprehenderit
+        voluptatum dolor tenetur nobis dolorem eum velit quos officiis natus
+        quisquam delectus numquam culpa!
+      </div>
     </div>
   );
 }
