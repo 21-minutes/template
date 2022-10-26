fe9d323b774da1dcdb458705a8b452c2a375a242
Janis Ringli <janis.ringli@gmail.com>
2022-10-26 15:51:17 +0200

    start development for searchbar

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,12 +1,25 @@
 import "./SearchInput.scss";
 import searchIcon from "../../assets/search.svg";
+import { useState } from "react";
 
 function SearchInput() {
+  const [input, setInput] = useState("");
+  function handleOnChange(event) {
+    setInput(event.target.value);
+    console.log(input);
+  }
   return (
     <div className="search-input__container">
       <div className="search-input__container__input">
         <img className="input-icon" src={searchIcon}></img>
-        <input className="input-text" placeholder="Search"></input>
+        <input
+          className="input-text"
+          placeholder="Search"
+          onChange={handleOnChange}
+          id="input"
+          name="input"
+          value={input}
+        ></input>
       </div>
     </div>
   );
