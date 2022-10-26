201683e262f94cc195a7ca93f2178b8ec21426bd
Janis Ringli <janis.ringli@gmail.com>
2022-10-26 15:50:23 +0200

    add button to home

--- src/Sections/Footer/Footer.jsx
+++ src/Sections/Footer/Footer.jsx
@@ -1,12 +1,16 @@
 import "./Footer.scss";
 import houseIcon from "../../assets/house.svg";
 import searchIcon from "../../assets/search.svg";
+import { Navigate } from "react-router-dom";
 
 function Footer() {
   return (
     <div className="footer-container">
       <div className="container-icons">
-        <img className="icon-item" src={houseIcon}></img>
+        <a href="/">
+          <img className="icon-item" src={houseIcon}></img>
+        </a>
+
         <img className="icon-item" src={searchIcon}></img>
 
         <div className="icon-item"></div>
