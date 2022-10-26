194904c516936914d47eec8b833c5785edca5c41
Janis Ringli <janis.ringli@gmail.com>
2022-10-26 15:54:07 +0200

    relace props with data from the api

--- src/components/ContentHeader/ContentHeader.jsx
+++ src/components/ContentHeader/ContentHeader.jsx
@@ -3,18 +3,37 @@ import linkedinIcon from "../../assets/linkedin.svg";
 import linkIcon from "../../assets/link.svg";
 import instagramIcon from "../../assets/instagram.svg";
 import avatar from "../../assets/people/janis_ringli.jpg";
+import { getAllUsers } from "../../Model/Author";
+import { getArticles } from "../../Model/Article";
+
+import { useEffect, useState } from "react";
 
 function ContentHeader(props) {
+  const [allUsers, setAllUsers] = useState();
+  const [article, setArticle] = useState();
+
+  useEffect(() => {
+    async function retrieveData() {
+      const authorData = await getAllUsers();
+      const articleData = await getArticles();
+
+      setAllUsers(authorData);
+      setArticle(articleData);
+    }
+    retrieveData();
+  }, []);
   return (
     <div className="content-header__container">
       <div className="author-section">
         <img className="author-image" src={avatar}></img>
         <div className="container__author">
-          <div className="author-name">{props.name}</div>
+          <div className="author-name">{allUsers?.attributes.name}</div>
           <div className="author-info">
             <div className="info-date">{props.date}</div>
             <div className="dot"></div>
-            <div className="info-reading-time">{props.readingTime} read</div>
+            <div className="info-reading-time">
+              {article?.attributes.readingTime} min read
+            </div>
           </div>
         </div>
       </div>
