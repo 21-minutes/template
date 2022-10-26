960d1798dbced495a8a19ab7e27b3f0e71864f23
Janis Ringli <janis.ringli@gmail.com>
2022-10-26 15:48:22 +0200

    relace props with data from the api

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,16 +1,31 @@
 import "./Article.scss";
 import ContentHeader from "../../components/ContentHeader/ContentHeader";
-function Article(props) {
+import { getArticles } from "../../Model/Article";
+import { useEffect, useState } from "react";
+
+function Article() {
+  const [article, setArticle] = useState();
+
+  useEffect(() => {
+    async function retrieveData() {
+      const articleData = await getArticles();
+      setArticle(articleData);
+    }
+    retrieveData();
+  }, []);
+
   return (
     //TODO: Replace content with api variables
-
     <div className="article__container">
       <div className="template-article">
-        <ContentHeader name="Janis Ringli" date="4 Oct" readingTime="5 Min" />
-        <div className="article-title">{props.title}</div>
-        <div className="article-content">{props.text}</div>
+        <ContentHeader date="4 Oct" readingTime="5 Min" />
+        <div className="container-content">
+          <div className="article-title">{article?.attributes.title}</div>
+          <div className="article-content">{article?.attributes.content}</div>
+        </div>
       </div>
     </div>
   );
 }
+
 export default Article;
