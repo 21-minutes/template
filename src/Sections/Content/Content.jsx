56023b079b822729817b555d4201da53257e356c
Janis Ringli <janis.ringli@gmail.com>
2022-10-26 15:48:54 +0200

    remove prop

--- src/Sections/Content/Content.jsx
+++ src/Sections/Content/Content.jsx
@@ -5,55 +5,7 @@ function Content() {
     <div className="content__container">
       <div className="template-content">
         <div className="content-header"></div>
-        <Article
-          title="This is the first Article"
-          text="Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          Voluptatum,qui voluptate dicta sequi dignissimos eaque modi at
-          dolorum, suscipit porro, ullam ea sunt? Ullam soluta pariatur sit,
-          adipisci quae obcaecati? Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Non molestiae officia minima! Vel neque dignissimos
-          consequatur voluptate at laboriosam similique quo, itaque, id placeat
-          deserunt omnis blanditiis totam? Nemo, modi! Lorem ipsum, dolor sit
-          amet consectetur adipisicing elit. Voluptatum,qui voluptate dicta
-          sequi dignissimos eaque modi at dolorum, suscipit porro, ullam ea
-          sunt? Ullam soluta pariatur sit, adipisci quae obcaecati? Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Non molestiae officia
-          minima! Vel neque dignissimos consequatur voluptate at laboriosam
-          similique quo, itaque, id placeat deserunt omnis blanditiis totam?
-          Nemo, modi! Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          Voluptatum,qui voluptate dicta sequi dignissimos eaque modi at
-          dolorum, suscipit porro, ullam ea sunt? Ullam soluta pariatur sit,
-          adipisci quae obcaecati? Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Non molestiae officia minima! Vel neque dignissimos
-          consequatur voluptate at laboriosam similique quo, itaque, id placeat
-          deserunt omnis blanditiis totam? Nemo, modi! Lorem ipsum, dolor sit
-          amet consectetur adipisicing elit. Voluptatum,qui voluptate dicta
-          sequi dignissimos eaque modi at dolorum, suscipit porro, ullam ea
-          sunt? Ullam soluta pariatur sit, adipisci quae obcaecati? Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Non molestiae officia
-          minima! Vel neque dignissimos consequatur voluptate at laboriosam
-          similique quo, itaque, id placeat deserunt omnis blanditiis totam?
-          Nemo, modi! Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          Voluptatum,qui voluptate dicta sequi dignissimos eaque modi at
-          dolorum, suscipit porro, ullam ea sunt? Ullam soluta pariatur sit,
-          adipisci quae obcaecati? Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Non molestiae officia minima! Vel neque dignissimos
-          consequatur voluptate at laboriosam similique quo, itaque, id placeat
-          deserunt omnis blanditiis totam? Nemo, modi! Lorem ipsum, dolor sit
-          amet consectetur adipisicing elit. Voluptatum,qui voluptate dicta
-          sequi dignissimos eaque modi at dolorum, suscipit porro, ullam ea
-          sunt? Ullam soluta pariatur sit, adipisci quae obcaecati? Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Non molestiae officia
-          minima! Vel neque dignissimos consequatur voluptate at laboriosam
-          similique quo, itaque, id placeat deserunt omnis blanditiis totam?
-          Nemo, modi! Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          Voluptatum,qui voluptate dicta sequi dignissimos eaque modi at
-          dolorum, suscipit porro, ullam ea sunt? Ullam soluta pariatur sit,
-          adipisci quae obcaecati? Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Non molestiae officia minima! Vel neque dignissimos
-          consequatur voluptate at laboriosam similique quo, itaque, id placeat
-          deserunt omnis blanditiis totam? Nemo, modi!"
-        />
+        <Article />
       </div>
     </div>
   );
