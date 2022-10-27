33e4ab32f453ca2446a93301fb4a9e26595b5aaf
Janis Ringli <janis.ringli@gmail.com>
2022-10-27 16:22:33 +0200

    add new api request to get one author

--- src/Model/Author.js
+++ src/Model/Author.js
@@ -1,11 +1,14 @@
 export async function getAllUsers() {
   try {
-    const response = await fetch("http://127.0.0.1:1337/api/authors/1", {
-      method: "GET",
-      // headers: new Headers({
-      //   Authorization: `bearer ${import.meta.env.VITE_REACT_APP_API_TOKEN}`,
-      // }),
-    });
+    const response = await fetch(
+      "http://127.0.0.1:1337/api/authors/1?populate=*",
+      {
+        method: "GET",
+        // headers: new Headers({
+        //   Authorization: `bearer ${import.meta.env.VITE_REACT_APP_API_TOKEN}`,
+        // }),
+      }
+    );
     const payload = await response.json();
     return payload.data;
   } catch (error) {
@@ -14,7 +17,7 @@ export async function getAllUsers() {
 }
 
 export async function createUser(data) {
-  const response = await fetch(`127.0.0.1:1337/api/authors/1`, {
+  const response = await fetch(`127.0.0.1:1337/api/authors/1?populate=*`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
