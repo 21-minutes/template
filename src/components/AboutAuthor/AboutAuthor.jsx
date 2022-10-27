4c8a0cd223c629f0d2803ef086e6f01a0e790e3b
Janis Ringli <janis.ringli@gmail.com>
2022-10-27 16:39:22 +0200

    replace hardcode with api code

--- src/components/AboutAuthor/AboutAuthor.jsx
+++ src/components/AboutAuthor/AboutAuthor.jsx
@@ -1,22 +1,36 @@
 import "./AboutAuthor.scss";
 import { getAllUsers } from "../../Model/Author";
+import { getArticles, getSingleArticle } from "../../Model/Article";
+
 import { useEffect, useState } from "react";
 import avatar from "../../assets/people/janis_ringli.jpg";
+import { useParams } from "react-router-dom";
 
 function AboutAuthor() {
   const [allUsers, setAllUsers] = useState();
+  const [article, setArticle] = useState();
+  let { articleId } = useParams();
+
   useEffect(() => {
     async function retrieveData() {
       const authorData = await getAllUsers();
+      const articleData = await getSingleArticle(articleId);
+
       setAllUsers(authorData);
+      setArticle(articleData);
     }
     retrieveData();
   }, []);
 
   return (
     <div className="about-author__container">
-      <img className="container-avatar" src={avatar}></img>
-      <div className="container-name">{allUsers?.attributes.name}</div>
+      <img
+        className="container-avatar"
+        src={`http://127.0.0.1:1337${article?.attributes.author.data?.attributes.avatar.data?.attributes.formats.medium.url}`}
+      ></img>
+      <div className="container-name">
+        {article?.attributes.author.data?.attributes.name}
+      </div>
       <div className="container-bio">{allUsers?.attributes.bio}</div>
     </div>
   );
--- src/components/ContentHeader/ContentHeader.jsx
+++ src/components/ContentHeader/ContentHeader.jsx
@@ -4,32 +4,37 @@ import linkIcon from "../../assets/link.svg";
 import instagramIcon from "../../assets/instagram.svg";
 import avatar from "../../assets/people/janis_ringli.jpg";
 import { getAllUsers } from "../../Model/Author";
-import { getArticles } from "../../Model/Article";
+import { getArticles, getSingleArticle } from "../../Model/Article";
+import { useParams } from "react-router-dom";
 
 import { useEffect, useState } from "react";
 
-function ContentHeader(props) {
+function ContentHeader() {
   const [allUsers, setAllUsers] = useState();
   const [article, setArticle] = useState();
+  let { articleId } = useParams();
 
   useEffect(() => {
     async function retrieveData() {
       const authorData = await getAllUsers();
-      const articleData = await getArticles();
+      const articleData = await getSingleArticle(articleId);
 
       setAllUsers(authorData);
       setArticle(articleData);
     }
     retrieveData();
   }, []);
+  console.log(article?.attributes.author.data?.attributes.name);
   return (
     <div className="content-header__container">
       <div className="author-section">
-        <img className="author-image" src={avatar}></img>
+        <img className="author-image" src={`http://127.0.0.1:1337${article?.attributes.author.data?.attributes.avatar.data?.attributes.formats.small.url}`}></img>
         <div className="container__author">
-          <div className="author-name">{allUsers?.attributes.name}</div>
+          <div className="author-name">
+            {article?.attributes.author.data?.attributes.name}
+          </div>
           <div className="author-info">
-            <div className="info-date">{props.date}</div>
+            <div className="info-date">{article?.attributes.date}</div>
             <div className="dot"></div>
             <div className="info-reading-time">
               {article?.attributes.readingTime} min read
