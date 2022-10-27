65d1fd54193ceb548874f73447d276769f7bc50f
Janis Ringli <janis.ringli@gmail.com>
2022-10-27 08:22:34 +0200

    replace data from local array with api data

--- src/components/RecommendedTopics/RecommendedTopics.jsx
+++ src/components/RecommendedTopics/RecommendedTopics.jsx
@@ -1,23 +1,27 @@
 import "./RecommendedTopics.scss";
+import React, { useEffect, useState } from "react";
+import { getTopics } from "../../Model/Topic";
 
+// TODO: Replace content with api variables
+// article?.attributes.topics?.data[0].attributes.name
 function RecommendedTopics() {
-  let topics = [
-    "Technology",
-    "Design",
-    "Culture",
-    "Business",
-    "Politics",
-    "Science",
-    "Health",
-    "Style",
-    "Travel",
-  ];
+  const [topic, setTopic] = useState([]);
+  useEffect(() => {
+    async function retrieveData() {
+      const topicData = await getTopics();
+      setTopic(topicData);
+    }
+    retrieveData();
+  }, []);
+
   return (
     <div className="recommended-topics__container">
       <div className="recommended-topics__title">Recommended Topics</div>
       <div className="recommended-topics__content">
-        {topics.map((topic) => (
-          <div className="topic-item">{topic}</div>
+        {topic.map((topic) => (
+          <div className="topic-item" key={topic.id}>
+            {topic?.attributes.name}
+          </div>
         ))}
       </div>
     </div>
