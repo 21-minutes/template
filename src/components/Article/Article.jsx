67b0a33d882019ed8cc23a96d2e2a4c6ff953ad8
Janis Ringli <janis.ringli@gmail.com>
2022-10-28 14:38:48 +0200

    change way my api works

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,35 +1,30 @@
 import "./Article.scss";
 import ContentHeader from "../../components/ContentHeader/ContentHeader";
-import { getArticles } from "../../Model/Article";
+import { getSingleArticle } from "../../Model/Article";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Article() {
-  const [article, setArticle] = useState([]);
+  const [article, setArticle] = useState();
 
   let { articleId } = useParams();
   useEffect(() => {
     async function retrieveData() {
-      const articleData = await getArticles();
+      const articleData = await getSingleArticle(articleId);
       setArticle(articleData);
     }
 
     retrieveData();
   }, []);
-  document.title = article[articleId - 1]?.attributes.title;
-
+  document.title = article?.attributes.title;
   return (
     //TODO: Replace content with api variables
     <div className="article__container">
       <div className="template-article">
         <ContentHeader date="4 Oct" />
         <div className="container-content">
-          <div className="article-title">
-            {article[articleId - 1]?.attributes.title}
-          </div>
-          <div className="article-content">
-            {article[articleId - 1]?.attributes.content}
-          </div>
+          <div className="article-title">{article?.attributes.title}</div>
+          <div className="article-content">{article?.attributes.content}</div>
         </div>
       </div>
     </div>
--- src/components/ContentHeader/ContentHeader.jsx
+++ src/components/ContentHeader/ContentHeader.jsx
@@ -2,7 +2,6 @@ import "./ContentHeader.scss";
 import linkedinIcon from "../../assets/linkedin.svg";
 import linkIcon from "../../assets/link.svg";
 import instagramIcon from "../../assets/instagram.svg";
-import avatar from "../../assets/people/janis_ringli.jpg";
 import { getAllUsers } from "../../Model/Author";
 import { getArticles, getSingleArticle } from "../../Model/Article";
 import { useParams } from "react-router-dom";
@@ -10,25 +9,27 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 function ContentHeader() {
-  const [allUsers, setAllUsers] = useState();
   const [article, setArticle] = useState();
   let { articleId } = useParams();
 
   useEffect(() => {
     async function retrieveData() {
-      const authorData = await getAllUsers();
       const articleData = await getSingleArticle(articleId);
 
-      setAllUsers(authorData);
       setArticle(articleData);
     }
     retrieveData();
   }, []);
-  console.log(article?.attributes.author.data?.attributes.name);
   return (
     <div className="content-header__container">
       <div className="author-section">
-        <img className="author-image" src={`http://127.0.0.1:1337${article?.attributes.author.data?.attributes.avatar.data?.attributes.formats.small.url}`}></img>
+        <img
+          className="author-image"
+          src={`${import.meta.env.VITE_REACT_APP_STRAPI_API_URL}${
+            article?.attributes.author.data?.attributes.avatar.data?.attributes
+              .formats.small.url
+          }`}
+        ></img>
         <div className="container__author">
           <div className="author-name">
             {article?.attributes.author.data?.attributes.name}
